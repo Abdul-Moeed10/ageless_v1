0feb6c7b115d16c2cb468b164e1bb60b77a5f1fc
Abdul Moeed <abdul.moeed1010@gmail.com>
2022-11-24 17:46:54 +0500

    Commit 2

--- app/src/main/java/com/example/ageless_v1/Sign_Up.java
+++ app/src/main/java/com/example/ageless_v1/Sign_Up.java
@@ -1,6 +1,9 @@
 package com.example.ageless_v1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,14 @@ public class Sign_Up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button proceed = findViewById(R.id.proceed_button);
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Sign_Up.this, Set_Password.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
