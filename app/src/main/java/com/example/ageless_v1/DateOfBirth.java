73f720e60af2c29df95c3b54a2cc23e72a78ddb6
Abdul-moeed10 <abdul.moeed1010@gmail.com>
2023-01-26 13:59:24 +0500

    Date of birth updated

--- app/src/main/java/com/example/ageless_v1/DateOfBirth.java
+++ app/src/main/java/com/example/ageless_v1/DateOfBirth.java
@@ -1,11 +1,6 @@
 package com.example.ageless_v1;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-
-import android.support.v8.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +9,17 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link DateOfBirth#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DateOfBirth<adapter> extends Fragment {
+public class DateOfBirth<adapter> extends Fragment implements DatePickerDialog.OnDateSetListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,6 +33,9 @@ public class DateOfBirth<adapter> extends Fragment {
     DatePicker picker;
     Button btnGet;
     TextView tvw;
+
+    TextView tvDate;
+    Button btPickDate;
 
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
@@ -70,13 +73,12 @@ public class DateOfBirth<adapter> extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-        tvw=(TextView)findViewById(R.id.textView1);
-        picker=(DatePicker)findViewById(R.id.datePicker1);
-        btnGet=(Button)findViewById(R.id.button1);
         btnGet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvw.setText("Selected Date: "+ picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear());
+                com.example.ageless_v1.DatePicker_Fragment mDatePickerDialogFragment;
+                mDatePickerDialogFragment = new com.example.ageless_v1.DatePicker_Fragment();
+                mDatePickerDialogFragment.show(getChildFragmentManager(), "DATE PICK");
             }
         });
     }
@@ -85,7 +87,21 @@ public class DateOfBirth<adapter> extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date_of_birth, container, false);
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_date_of_birth, container, false);
+        tvDate = viewGroup.findViewById(R.id.textView1);
+        picker = viewGroup.findViewById(R.id.datePicker1);
+        btnGet = viewGroup.findViewById(R.id.button1);
+        return viewGroup;
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar mCalendar = Calendar.getInstance();
+        mCalendar.set(Calendar.YEAR, year);
+        mCalendar.set(Calendar.MONTH, month);
+        mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+        tvDate.setText(selectedDate);
     }
 }
---
+++ app/src/main/java/com/example/ageless_v1/DatePicker_Fragment.java
@@ -0,0 +1,41 @@
+package com.example.ageless_v1;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the factory method to
+ * create an instance of this fragment.
+ */
+public class DatePicker_Fragment extends DialogFragment {
+
+    public DatePicker_Fragment() {
+        // nothing to see here, move along
+    }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar mCalendar = Calendar.getInstance();
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int dayOfMonth = mCalendar.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener)
+                getActivity(), year, month, dayOfMonth);
+    }
+}
+
+
+
