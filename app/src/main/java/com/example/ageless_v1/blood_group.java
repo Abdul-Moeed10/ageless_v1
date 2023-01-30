2672f417f078def166291176920662c0f21c69b2
Abdul-moeed10 <abdul.moeed1010@gmail.com>
2023-01-31 03:45:14 +0500

    Splash screen, app icon, etc.

--- app/src/main/java/com/example/ageless_v1/blood_group.java
+++ app/src/main/java/com/example/ageless_v1/blood_group.java
@@ -38,13 +38,13 @@ public class blood_group extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_blood_group, container, false);
         A_plus = viewGroup.findViewById(R.id.A_plus);
-//        B_plus = viewGroup.findViewById(R.id.B_plus);
-//        O_plus = viewGroup.findViewById(R.id.O_plus);
-//        AB_plus = viewGroup.findViewById(R.id.AB_plus);
-//        A_minus = viewGroup.findViewById(R.id.A_minus);
-//        B_minus = viewGroup.findViewById(R.id.B_minus);
-//        O_minus = viewGroup.findViewById(R.id.O_minus);
-//        AB_minus = viewGroup.findViewById(R.id.AB_minus);
+        B_plus = viewGroup.findViewById(R.id.B_plus);
+        O_plus = viewGroup.findViewById(R.id.O_plus);
+        AB_plus = viewGroup.findViewById(R.id.AB_plus);
+        A_minus = viewGroup.findViewById(R.id.A_minus);
+        B_minus = viewGroup.findViewById(R.id.B_minus);
+        O_minus = viewGroup.findViewById(R.id.O_minus);
+        AB_minus = viewGroup.findViewById(R.id.AB_minus);
         CurrentProgress = 70;
         progressBar = getActivity().findViewById(R.id.progressBar);
 
@@ -66,110 +66,110 @@ public class blood_group extends Fragment {
             }
         });
 
-//        A_minus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "A-";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        B_plus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "B+";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        B_minus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "B-";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        O_plus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "O+";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        O_minus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "O-";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        AB_plus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "AB+";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
-//
-//        AB_minus.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String blood_group = "AB-";
-//                add_data.putString("blood_group", blood_group);
-//                add_data.apply();
-//                Fragment fragment = new emergency_contact_link();
-//                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-//                        .commit();
-//                progressBar.setProgress(CurrentProgress);
-//            }
-//        });
+        A_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "A-";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        B_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "B+";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        B_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "B-";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        O_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "O+";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        O_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "O-";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        AB_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "AB+";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
+
+        AB_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String blood_group = "AB-";
+                add_data.putString("blood_group", blood_group);
+                add_data.apply();
+                Fragment fragment = new emergency_contact_link();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_view, fragment)
+                        .commit();
+                progressBar.setProgress(CurrentProgress);
+            }
+        });
 
 
 
