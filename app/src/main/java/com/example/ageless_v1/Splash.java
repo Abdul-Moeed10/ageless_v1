10898045724959d0d9241b4b4b9422da9a7aaadc
Abdul-moeed10 <abdul.moeed1010@gmail.com>
2023-06-30 20:59:33 +0500

    Integrated fall model

--- app/src/main/java/com/example/ageless_v1/Splash.java
+++ app/src/main/java/com/example/ageless_v1/Splash.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Splash extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
     Handler handler;
 
     @Override
@@ -16,15 +19,40 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(Splash.this, Login.class);
-                startActivity(intent);
-                finish();
-            }
-        },500);
+        mAuth = FirebaseAuth.getInstance();
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if the user is already logged in
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            // User is already logged in, open the desired activity
+            handler=new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(Splash.this, Dashboard.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },500);
+        } else {
+            // User is not logged in, open the login activity
+            handler=new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(Splash.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },500);
+        }
+    }
+
 }
